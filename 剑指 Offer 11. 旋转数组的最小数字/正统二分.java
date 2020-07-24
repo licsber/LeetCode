1f47e714b74a144dd9977abaf1e3963e440398da
licsber <silverwings@foxmail.com>
2020-07-24 22:37:19 +0800

    update by licsber

---
+++ 剑指 Offer 11. 旋转数组的最小数字/正统二分.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minArray(int[] numbers) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (numbers[mid] < numbers[right]) {
+                right = mid;
+            } else if (numbers[mid] > numbers[right]) {
+                left = mid + 1;
+            } else {
+                --right;
+            }
+        }
+        return numbers[left];
+    }
+}
