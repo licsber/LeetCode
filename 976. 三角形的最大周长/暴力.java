3cd8520025ce9268a5ddec82ae83f8d0defdc732
licsber <silverwings@foxmail.com>
2020-11-29 12:13:52 +0800

    update by licsber

--- 976. 三角形的最大周长/暴力.java
+++ 976. 三角形的最大周长/暴力.java
@@ -4,16 +4,15 @@ class Solution {
     public int largestPerimeter(int[] A) {
         int[] nums = Arrays.copyOf(A, A.length);
         Arrays.sort(nums);
-        int res = 0;
         for (int i = nums.length - 1; i >= 2; --i) {
             int a = nums[i - 2];
             int b = nums[i - 1];
             int c = nums[i];
             if (valid(a, b, c)) {
-                res = Math.max(res, a + b + c);
+                return a + b + c;
             }
         }
-        return res;
+        return 0;
     }
 
     private boolean valid(int a, int b, int c) {
